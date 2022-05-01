717ac2cc955c03e2849577152ac04a15bb4cf75c
Joshua Hoeflich <joshuaharry411@icloud.com>
2022-04-30 21:06:48 -0500

    Adds uncaught exception debugging

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@
 import { parseArgv } from "./argParse";
 import { interpret } from "./interpret";
 
+// TODO: Periodically, node is throwing errors related to closing streams.
+// I'm not sure why this is happening, so in the meantime, we'll log the
+// exceptions and try to keep going.
+process.on("uncaughtException", function (err) {
+  console.error(err);
+  console.error(err.stack);
+});
+
 if (require.main === module) {
   const req = parseArgv(process.argv);
   interpret(req);
