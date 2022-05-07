73d076d9ee5e62540c7b80ba79e1c8f304119053
Joshua Hoeflich <joshuaharry411@icloud.com>
2022-05-07 09:28:22 -0500

    Clean

--- src/interpret.test.ts
+++
@@ -1,7 +0,0 @@
-import { toValidUnixName } from "./interpret";
-
-describe("Valid unix names", () => {
-  test("Succeed as desired", () => {
-    expect(toValidUnixName("@test/hello")).toEqual("@test-hello");
-  });
-});
