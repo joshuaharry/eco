44c72de5ca964555aa21d90f8ff5df2ae419a652
Manuel Serrano <Manuel.Serrano@inria.fr>
2022-07-27 08:49:59 +0200

    .

--- lists/eco-all-lists.sh
+++ lists/eco-all-lists.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
-# split a list of packages
-
+# run eco on the split lists
+#    usage: eco-all-lists.sh [index]
+#    index, if provided should be a multple of 100
 all=dt-all
+
+if [ !-f dt-all ]; then
+  echo "*** ERROR: cannot find dt-all, should run in the list directory."
+  exit 1;
+fi
+
+if [ "$1" != "" ]; then
+  i=$1
+else
+  i=0
+fi
 
 size=100
 num=`wc -l $all | awk '{print $1}'`
 
-i=0
 
 while expr $i "<" $num; do
   echo "====== eco -n -s strategies/scotty.json -f dt-all.$i -d DT-ALL.$i"
