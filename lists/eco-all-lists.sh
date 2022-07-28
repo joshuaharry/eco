3c284b13ad33d790f88c0fe922f63302c31a2377
Manuel Serrano <Manuel.Serrano@inria.fr>
2022-07-28 18:46:51 +0200

    New eco-all-lists.sh option

--- lists/eco-all-lists.sh
+++ lists/eco-all-lists.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 # run eco on the split lists
-#    usage: eco-all-lists.sh [index]
+#    usage: eco-all-lists.sh [-n] [index]
 #    index, if provided should be a multple of 100
 all=dt-all
+cleanup=
 
 if [ ! -f dt-all ]; then
   echo "*** ERROR: cannot find dt-all, should run in the list directory."
   exit 1;
 fi
+
+if [ "$1 " = "-n " ]; then
+  cleanup=-n
+  shift
+fi  
 
 if [ "$1" != "" ]; then
   i=$1
@@ -18,10 +24,9 @@ fi
 size=100
 num=`wc -l $all | awk '{print $1}'`
 
-
 while expr $i "<" $num; do
-  echo "====== eco -n -s strategies/scotty.json -f dt-all.$i -d DT-ALL.$i"
-  eco -s ../strategies/scotty.json -f dt-all.$i -d DT-ALL.$i
+  echo "====== eco $cleanup -s strategies/scotty.json -f dt-all.$i -d DT-ALL.$i"
+  eco $cleanup -s ../strategies/scotty.json -f dt-all.$i -d DT-ALL.$i
   i=`expr $i "+" 100`
 done  
 
