e56ca29e0253602e7e9891ab6d11e4273b01a2b9
Manuel Serrano <Manuel.Serrano@inria.fr>
2022-07-30 06:07:13 +0200

    Fix split.sh

--- lists/split.sh
+++ lists/split.sh
@@ -8,10 +8,21 @@ num=`wc -l $all | awk '{print $1}'`
 
 i=0
 j=100
+cnt=100
 
-while expr $i "<" $num; do
-  head -n $j dt-all | tail -n 100 > dt-all.$i
+while expr $i "<" $num > /dev/null; do
+  cnt=`expr $num "-" $i`
+
+  if expr $cnt ">" 100 > /dev/null; then
+    cnt=100
+  elif expr $cnt "<" 0 > /dev/null; then
+    cnt=`expr $i "-" $num`
+  fi
+
+  echo "dt-all.$i $j=$j cnt=$cnt"
+  
+  head -n $j dt-all | tail -n $cnt > dt-all.$i
   i=`expr $i "+" 100`
-  j=`expr $j "+" 100`
+  j=`expr $j "+" $cnt`
 done  
 
