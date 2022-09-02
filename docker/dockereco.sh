d05322a555ad1f343b1f3eb0183614a37770fbb8
Manuel Serrano <Manuel.Serrano@inria.fr>
2022-09-02 16:22:16 +0200

    Add a simple bash script for running inside docker

--- docker/dockereco.sh
+++ docker/dockereco.sh
@@ -2,21 +2,77 @@
 # run eco inside docker
 
 image=eco
-lib=$1
-strategy=scotty
-dir=`dirname $0`
+libs=
+strategy=scotty.json
+logdir=`date +"%Y-%Hh%M"`
+ecodir=`dirname $0`
 
-if [ "$2 " != " " ];then
-  strategy = $2
-fi
+# argument parsing
+while : ; do
+  case $1 in
+    "")
+      break;;
+      
+    -s|--strategy)
+      shift;
+      strategy = $1;;
+
+    -n|--no-cleanup)
+      cleanup = "-n";;
+
+    -j|--cpus)
+      shift
+      cpus = $1;;
+
+    -d|--log-dir)
+      shift
+      logdir = $1;;
+
+    -v|--verbose)
+      verbose = "-v";;
+
+    -*)
+      echo "Unknown argument" >&2;
+      exit 1;;
+
+    *)
+      libs="$libs $1"
+      
+  esac
+  shift
+done
+
+strategydir=`basename $strategy .json`
+
+logpath=$HOME/.eco/$strategydir/$logdir
+mkdir -p $logpath
 
 # check if the docker image exists
-if [`docker image inspect --format="-" $image` != "-" ]; then
-  echo "building docker image  [$image]"
-  docker build -f $dir/Dockerfile -t $image
+if [ "`docker image inspect --format=- $image`" != "-" ]; then
+  (cd $ecodir; docker build . -t $image)
 fi  
 
+# one run
+run() {
+  lib=$1
+
+  if [ "$verbose " != "-v " ]; then
+    echo -n "$image.$lib..."
+    docker run --name $image.$lib $image -c "$image $verbose -s /home/scotty/.$image/strategies/$strategy -n -d LOG $lib" > /dev/null
+  else
+    echo "$image.$lib..."
+    docker run -a STDOUT -a STDERR --name $image.$lib $image -c "$image $verbose -s /home/scotty/.$image/strategies/$strategy -n -d LOG $lib"
+  fi
+  
+  docker cp eco.$lib:/home/scotty/.eco/$strategydir/LOG/$lib $logpath
+  echo "[$logpath/$lib]"
+  
+  if [ "$cleanup " != "-n" ]; then
+    docker rm -f $image.$lib > /dev/null
+  fi
+}
+
 # run the container
-echo "running docker container [$image.$lib]"
-docker run -a STDOUT -a STDERR --name $image.$lib $image -c "$image -v -s /home/scotty/.$image/strategies/$strategy -n -d LOG $lib"
-docker rm -f $image.$lib
+for lib in $libs; do
+  run $lib
+done  
